1c739fad9133471b1b1e781ccb5ffd3caab88366
ivan <6_6_6_66@maik.ru>
2024-10-09 19:59:19 +0300

    Menu realization

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/es-debug/backend-academy-2024-go-template
 
 go 1.22.6
 
-require golang.org/x/exp v0.0.0-20241004190924-225e2abe05e6
+require (
+	github.com/buger/goterm v1.0.4
+	github.com/pkg/term v1.1.0
+	golang.org/x/exp v0.0.0-20241004190924-225e2abe05e6
+)
+
+require golang.org/x/sys v0.0.0-20210331175145-43e1dd70ce54 // indirect
---
+++ pkg/menu.go
@@ -0,0 +1,138 @@
+package pkg
+
+import (
+	"fmt"
+
+	"github.com/buger/goterm"
+	"github.com/pkg/term"
+)
+
+const (
+	up    byte = 65
+	down  byte = 66
+	enter byte = 13
+)
+
+var keys = map[byte]bool{
+	up:   true,
+	down: true,
+}
+
+type Menu struct {
+	Prompt         string
+	CursorPosition int
+	MenuItems      []*MenuItem
+}
+
+type MenuItem struct {
+	Text    string
+	SubMenu *Menu
+}
+
+func NewMenu(prompt string) *Menu {
+	return &Menu{
+		Prompt:    prompt,
+		MenuItems: make([]*MenuItem, 0),
+	}
+}
+
+func (m *Menu) AddItem(option string) *Menu {
+	menuItem := &MenuItem{
+		Text: option,
+	}
+
+	m.MenuItems = append(m.MenuItems, menuItem)
+	return m
+}
+
+func (m *Menu) renderMenuItems(redraw bool) {
+	if redraw {
+		fmt.Printf("\033[%dA", len(m.MenuItems)-1)
+	}
+
+	for index, menuItem := range m.MenuItems {
+		var newline = "\n"
+		if index == len(m.MenuItems)-1 {
+			newline = ""
+		}
+
+		menuItemText := menuItem.Text
+		cursor := "  "
+		if index == m.CursorPosition {
+			cursor = goterm.Color("> ", goterm.YELLOW)
+			menuItemText = goterm.Color(menuItemText, goterm.YELLOW)
+		}
+
+		fmt.Printf("\r%s %s%s", cursor, menuItemText, newline)
+	}
+}
+
+func (m *Menu) Display() (int, error) {
+	if len(m.MenuItems) == 0 {
+		return 0, nil
+	}
+
+	defer func() {
+		// Show cursor again.
+		fmt.Printf("\033[?25h")
+	}()
+
+	fmt.Printf("%s\n", goterm.Color(goterm.Bold(m.Prompt)+":", goterm.CYAN))
+
+	m.renderMenuItems(false)
+
+	// Turn the terminal cursor off
+	fmt.Printf("\033[?25l")
+
+	for {
+		keyCode, err := getInput()
+		if err != nil {
+			return 0, fmt.Errorf("getting input: %w", err)
+		}
+		if keyCode == enter {
+			fmt.Println("\r")
+			return m.CursorPosition, nil
+		} else if keyCode == up {
+			m.CursorPosition = (m.CursorPosition + len(m.MenuItems) - 1) % len(m.MenuItems)
+			m.renderMenuItems(true)
+		} else if keyCode == down {
+			m.CursorPosition = (m.CursorPosition + 1) % len(m.MenuItems)
+			m.renderMenuItems(true)
+		}
+	}
+}
+
+// getInput will read raw input from the terminal
+// It returns the raw ASCII value inputted
+func getInput() (byte, error) {
+	t, err := term.Open("/dev/tty")
+	if err != nil {
+		return 0, fmt.Errorf("opening terminal: %w", err)
+	}
+
+	err = term.RawMode(t)
+	if err != nil {
+		return 0, fmt.Errorf("setting raw mode: %w", err)
+	}
+
+	readBytes := make([]byte, 3)
+	read, err := t.Read(readBytes)
+	if err != nil {
+		return 0, fmt.Errorf("reading from terminal: %w", err)
+	}
+
+	t.Restore()
+	t.Close()
+
+	// Arrow keys are prefixed with the ANSI escape code which take up the first two bytes.
+	// The third byte is the key specific value we are looking for.
+	if read == 3 {
+		if _, ok := keys[readBytes[2]]; ok {
+			return readBytes[2], nil
+		}
+	} else {
+		return readBytes[0], nil
+	}
+
+	return 0, nil
+}
