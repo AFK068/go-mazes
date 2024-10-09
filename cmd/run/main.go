165d3578ddd545e7f6d77835d53c49eeda655921
ivan <6_6_6_66@mail.ru>
2024-10-09 19:59:19 +0300

    Menu realization

--- cmd/run/main.go
+++ cmd/run/main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"github.com/es-debug/backend-academy-2024-go-template/internal/domain"
-)
-
 func main() {
-	mazeKruskal := domain.KruskalGenerator{}
-	//mazePrims := domain.PrimGenerator{}
 
-	generateMazeKruskal := domain.NewGenerateMaze(&mazeKruskal)
-	//generateMazePrims := domain.NewGenerateMaze(&mazePrims)
-
-	maze := generateMazeKruskal.GenerateMaze(15, 50)
-	//maze := generateMazePrims.GenerateMaze(15, 50)
-
-	maze.PrintMaze()
 }
