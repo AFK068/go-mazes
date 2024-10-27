eebcea00f7fda66a65ff000c737b692342766030
ivan <6_6_6_66@maik.ru>
2024-10-27 15:49:51 +0300

    Added logging throughout the code and changed user input handling using the promptui library

--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.22.6
 
 require (
 	github.com/buger/goterm v1.0.4
+	github.com/manifoldco/promptui v0.9.0
 	github.com/pkg/term v1.1.0
 	golang.org/x/exp v0.0.0-20241004190924-225e2abe05e6
 )
 
-require golang.org/x/sys v0.26.0 // indirect
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	golang.org/x/sys v0.26.0 // indirect
+)
--- internal/application/maze_builder.go
+++ internal/application/maze_builder.go
@@ -2,15 +2,18 @@ package application
 
 import (
 	"fmt"
+	"log/slog"
 
 	"github.com/es-debug/backend-academy-2024-go-template/internal/domain"
 	"github.com/es-debug/backend-academy-2024-go-template/internal/infrastructure"
 )
 
-func InitializeMaze() error {
+func InitializeMaze() {
 	width, height, err := infrastructure.GetAndRoundWidthAndHeightFromUser()
 	if err != nil {
-		return fmt.Errorf("getting width and height: %w", err)
+		slog.Error("getting width and height maze size", slog.String("error", err.Error()))
+		fmt.Println("Failed to get width and height from user:", err)
+		return
 	}
 
 	width = width - width%2
@@ -19,17 +22,23 @@ func InitializeMaze() error {
 	// Controling that size maze is not bigger than console size
 	err = GetConsoleSize(width, height)
 	if err != nil {
-		return fmt.Errorf("getting console size: %w", err)
+		slog.Error("getting console size", slog.String("error", err.Error()))
+		fmt.Println("Failed to get console size:", err)
+		return
 	}
 
 	startX, startY, err := infrastructure.GetCoordinatesFromUser(width-1, height-1)
 	if err != nil {
-		return fmt.Errorf("getting start coordinates: %w", err)
+		slog.Error("getting start coordinates", slog.String("error", err.Error()))
+		fmt.Println("Failed to get start coordinates from user:", err)
+		return
 	}
 
 	endX, endY, err := infrastructure.GetCoordinatesFromUser(width-1, height-1)
 	if err != nil {
-		return fmt.Errorf("getting end coordinates: %w", err)
+		slog.Error("getting end coordinates", slog.String("error", err.Error()))
+		fmt.Println("Failed to get end coordinates from user:", err)
+		return
 	}
 
 	startCell := domain.NewCell(startY-1, startX-1, nil)
@@ -37,8 +46,17 @@ func InitializeMaze() error {
 
 	generator, err := selectGenerator()
 	if err != nil {
-		return fmt.Errorf("selecting generator: %w", err)
+		slog.Error("selecting generator", slog.String("error", err.Error()))
+		fmt.Println("Failed to select generator:", err)
+		return
 	}
+
+	slog.Info(
+		"generating maze", slog.Int("height", height),
+		slog.Int("width", width), slog.Int("start_x", startX),
+		slog.Int("start_y", startY), slog.Int("end_x", endX),
+		slog.Int("end_y", endY), slog.String("generator", fmt.Sprintf("%T", generator)),
+	)
 
 	generateMaze := domain.NewGenerateMaze(generator)
 	maze := generateMaze.GenerateMaze(height, width, startCell, endCell)
@@ -46,17 +64,21 @@ func InitializeMaze() error {
 
 	solver, err := selectSolver()
 	if err != nil {
-		return fmt.Errorf("selecting solver: %w", err)
+		slog.Error("selecting solver", slog.String("error", err.Error()))
+		fmt.Println("Failed to select solver:", err)
+		return
 	}
+
+	slog.Info("solving maze algorithm", slog.String("solver", fmt.Sprintf("%T", solver)))
 
 	found, path := solver.Solve(maze)
 	infrastructure.RenderMazeWithGridStepsWithDelay(&path)
 
 	if found {
+		slog.Info("path found in maze")
 		fmt.Println("Path found!")
 	} else {
+		slog.Info("no path found in maze")
 		fmt.Println("No path found.")
 	}
-
-	return nil
 }
